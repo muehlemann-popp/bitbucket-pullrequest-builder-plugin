33ce0b3889350c4b9b7cb483ae0ffd05da26cbd0
Dmitri Pluschaev <dmitri.pluschaev@muehlemann-popp.ch>
2018-08-02 18:18:47 +0300

    Extend3: cleanup result if build numbers don't match

--- src/main/java/bitbucketpullrequestbuilder/bitbucketpullrequestbuilder/BitbucketBuildListener.java
+++ src/main/java/bitbucketpullrequestbuilder/bitbucketpullrequestbuilder/BitbucketBuildListener.java
@@ -42,7 +42,7 @@ public class BitbucketBuildListener extends RunListener<Run<?, ?>> {
                 logger.fine("BitbucketBuildListener cannot retrieve environment variables.");
             }
 
-            BuildResult buildResult = (new BuildResultLoader(r, envVars)).getBuildResult();
+            BuildResult buildResult = (new BuildResultLoader(envVars)).getBuildResult();
 
             builds.onCompleted(
                     (BitbucketCause) r.getCause(BitbucketCause.class),
